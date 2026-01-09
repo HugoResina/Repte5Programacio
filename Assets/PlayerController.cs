46e7cee81aea9de896004ebee4c903daae1c51bf
Hugo <hugo.resina.7e7@itb.cat>
2026-01-09 17:38:23 +0100

    player controller new input system

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class PlayerController : MonoBehaviour {
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour ,InputSystem_Actions.IPlayerActions
+{
     private float horizontalMove;
     private float verticalMove;
     public CharacterController player;
@@ -14,12 +16,42 @@ public class PlayerController : MonoBehaviour {
  
     void Update () {
         //old input sistem
-        horizontalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");
+        //horizontalMove = Input.GetAxis("Horizontal");
+        //verticalMove = Input.GetAxis("Vertical");
 
     }
     private void FixedUpdate()
     {
         player.Move(new Vector3(horizontalMove, 0, verticalMove) * playerSpeed * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
     }
 }
